b38b4868031c1c0b514ec3c8867e6d01d02883b5
dayOneAbu <abubker.init@gmail.com>
2022-09-08 05:14:09 -0700

    fix for ex 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,5 +12,4 @@ int maini(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
-	return (0);
 }
