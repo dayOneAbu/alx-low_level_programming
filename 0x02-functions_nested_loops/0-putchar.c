0e88a50cd6dd9ac41d74debb3036c4b10eecc0c6
dayOneAbu <abubker.init@gmail.com>
2022-09-13 13:16:42 -0700

    task0-11

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * main -  prints putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_putchar');
+	_putchar('\n');
+
+	return (0);
+}
